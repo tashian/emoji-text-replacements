0a197e88f717a67613a15837ff50420c3a3fa20a
tashian <carl@tashian.com>
2017-04-25 11:36:36 -0700

    Add obvious replacements like :P or :)

--- index.js
+++ index.js
@@ -1,8 +1,42 @@
-const emojis = require('emojis-keywords');
-const emojis_list = require('emojis-list');
+function escapeXML(str) {
+    return str.replace(/&/g, '&amp;')
+               .replace(/</g, '&lt;')
+               .replace(/>/g, '&gt;')
+               .replace(/"/g, '&quot;')
+               .replace(/'/g, '&apos;');
+}
+
+const emoji_keywords = require('emojis-keywords');
+const emoji_list = require('emojis-list');
 const fs = require('fs');
 const _ = require('lodash');
-const all_emojis = _.zip(emojis, emojis_list);
+var all_emojis = _.zip(emoji_keywords, emoji_list);
+
+var obvious_additions = [
+    ["D:", "😧"],
+    [":'(", "😢"],
+    ["=)", "😃"],
+    ["=-)", "😃"],
+    [":D", "😄"],
+    [":-D", "😄"],
+    [";)", "😉"],
+    [";-)", "😉"],
+    [":o", "😮"],
+    [":-o", "😮"],
+    [":)", "🙂"],
+    [":-)", "🙂"],
+    ["(:", "🙂"],
+    [";P", "😜"],
+    [";-P", "😜"],
+    [";p", "😜"],
+    [";-p", "😜"],
+    [":P", "😛"],
+    [":-P", "😛"],
+    [":p", "😛"],
+    [":-p", "😛"]
+]
+
+all_emojis = all_emojis.concat(obvious_additions);
 
 var xml = `<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
@@ -15,14 +49,15 @@ _.each(all_emojis, function(emoji) {
         <key>phrase</key>
         <string>` + emoji[1] + `</string>
         <key>shortcut</key>
-        <string>` + emoji[0] + `</string>
+        <string>` + escapeXML(emoji[0]) + `</string>
     </dict>`
 })
 
-xml += `</array>
+xml += `
+</array>
 </plist>`;
 
-fs.writeFile('emoji.plist', xml, (err) => {
+fs.writeFile('dist/emoji.plist', xml, (err) => {
   if (err) throw err;
   console.log('The file has been saved!');
 })
